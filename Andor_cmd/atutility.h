9aaba0186be6d55bf60eb2f80e6530a3bc4ad590
kaitian <itiankai@qq.com>
2016-11-26 01:31:46 +0800

    Andor_cmd初始化提交

---
+++ Andor_cmd/atutility.h
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+
+#ifndef atutilityH
+#define atutilityH
+//---------------------------------------------------------------------------
+#include "atcore.h"
+
+#define AT_ERR_INVALIDOUTPUTPIXELENCODING 1002
+#define AT_ERR_INVALIDINPUTPIXELENCODING 1003
+#define AT_ERR_INVALIDMETADATAINFO 1004
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int AT_EXP_CONV AT_ConvertBuffer(AT_U8* inputBuffer,
+                                            AT_U8* outputBuffer,
+                                            AT_64 width,
+                                            AT_64 height,
+                                            AT_64 stride,
+                                            const AT_WC * inputPixelEncoding,
+                                            const AT_WC * outputPixelEncoding);
+int AT_EXP_CONV AT_ConvertBufferUsingMetadata(AT_U8* inputBuffer,
+                                            AT_U8* outputBuffer,
+                                            AT_64 imagesizebytes,
+                                            const AT_WC * outputPixelEncoding);
+int AT_EXP_CONV AT_InitialiseUtilityLibrary();
+int AT_EXP_CONV AT_FinaliseUtilityLibrary();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
